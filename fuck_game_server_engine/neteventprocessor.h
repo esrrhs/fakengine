fb980d3fd7ccc6f8a9c81508f90e1df127f51569
esrrhs <esrrhs@esrrhs-PC>
2012-05-04 18:42:14 +0800

    add

---
+++ fuck_game_server_engine/neteventprocessor.h
@@ -0,0 +1,34 @@
+#pragma once
+
+template <typename _ele_type, typename _msg>
+class neteventprocessor
+{
+public:
+	neteventprocessor()
+	{
+	}
+	~neteventprocessor()
+	{
+	}
+public:
+	force_inline bool on_close(_ele_type & e)
+	{
+		return true;
+	}
+	force_inline bool on_accept(_ele_type & e)
+	{
+		return true;
+	}
+	force_inline bool on_connect(_ele_type & e)
+	{
+		return true;
+	}
+	force_inline bool on_recv_msg(_ele_type & e, const _msg & msg)
+	{
+		return true;
+	}
+private:
+};
+
+
+
