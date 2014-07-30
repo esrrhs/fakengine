0f6fadb222dff5873e97624f3507b8b3539cd8e3
esrrhs <esrrhs@localhost>
2014-07-30 13:47:19 +0800

    add

--- fuck_game_server_engine/fversion.h
+++ fuck_game_server_engine/fversion.h
@@ -169,6 +169,7 @@ static const stringc version_logs[] = {
 	"2014.7.26：加fproto动态解析",
 	"2014.7.26：加fproto校验enum",
 	"2014.7.28：fix linux build",
+	"2014.7.28：加fproto校验struct,下一步加生成实际的对象体",
 };
 
 static force_inline int32_t fversion()
